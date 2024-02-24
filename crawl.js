d38fbee2037e35d7da122a455ba163a10f32e23e
Hari Prasad <81459630+haryprasadh@users.noreply.github.com>
2024-02-24 11:40:33 +0000

    added main crawler function : crawlPage()

--- crawl.js
+++ crawl.js
@@ -1,33 +1,47 @@
 const { JSDOM } = require("jsdom");
 
-
+async function crawlPage(currentURL) {
+  console.log(`wait, crawling now on ${currentURL} 
+ ......`);
+  try{
+    let res = await fetch(currentURL);
+    if(res.status>399){
+      console.log(`error in fetch with statuscode: ${res.status} on page ${currentURL}`);
+      return;
+    }
+    if(!res.headers.get('content-type').includes('text/html')){
+      console.log(`response is not html, it is ${res.headers.get('content-type')}, on page ${currentURL}`);
+      return;
+    }
+    console.log(await res.text());
+  }catch(err){
+    console.log(`error in fetch: ${err.message} on page : ${currentURL}`);
+  }
+}
 
 function getURLsFromHTML(htmlBody, baseURL) {
   let urls = [];
   let dom = new JSDOM(htmlBody);
-  let links = 
-  dom.window.document.getElementsByTagName("a");
+  let links = dom.window.document.getElementsByTagName("a");
   for (let link of links) {
-    if(link.href.slice(0,1)==='/'){
-      try{
+    if (link.href.slice(0, 1) === "/") {
+      try {
         let urlObj = new URL(`${baseURL}${link.href}`);
         urls.push(urlObj.href);
-      }catch(err){
+      } catch (err) {
         console.log(`error with the url: ${err.message}`);
       }
-    }else{
-      try{
+    } else {
+      try {
         let urlObj = new URL(link.href);
         urls.push(urlObj.href);
-      }catch(err){
+      } catch (err) {
         console.log(`error with the url: ${err.message}`);
       }
     }
   }
   return urls;
 }
-
-
 
 function normalizeURL(urlString) {
   let urlObj = new URL(urlString);
@@ -41,4 +55,5 @@ function normalizeURL(urlString) {
 module.exports = {
   normalizeURL,
   getURLsFromHTML,
+  crawlPage,
 };
